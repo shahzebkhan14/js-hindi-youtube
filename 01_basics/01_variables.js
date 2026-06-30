c8c60bb2d42096a3d87a2887537548dbdcec589b
shahzebkhan14 <shazebkhan450331@gmail.com>
2026-06-30 19:34:18 +0000

    ,

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId =144553
+let accountEmail = "shahzebkhan@gmail.com"
+var accountPassword = "123456"
+accountcity ="Jaipur" 
+let accountstate;
+
+ //accountId = 2
+
+
+accountEmail ="hc@hc.com"
+accountPassword ="21212121"
+accountcity = "Bengaluru"
+console.table ([accountId,accountEmail,accountPassword,accountcity,accountstate])
+
+console.log(accountId)
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scopee
+*/
