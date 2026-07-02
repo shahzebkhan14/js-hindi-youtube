a46d0639a14187faaf72b9c45b39c84cb7f3e0b1
shahzebkhan14 <shazebkhan450331@gmail.com>
2026-07-02 13:24:12 +0000

    data types

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,29 @@
-console.log(shahzebkhan);
+"use strict"; //Treat all Js Code As newer version
+
+// alert(3+3) // we are using Nodejs,not browser
+
+console.log(3
+    +
+    3) //code readblity should be high
+ 
+    console.log("shahzeb")
+
+
+let name = "shahzeb"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to pwor 63
+// bigint (used in big website  like trading website)
+//  string => ""
+// boolen => true/false (ya to h ya to ni h )
+//null=> standalone value for eg hamne temprture check kia aur website p ni h to vo 0 nhi null show krega
+// undefined => abhi value define ni hui h
+// symbol=> unique
+
+
+// object 
+
+console.log(typeof "undefined" ); // undefined
+console.log(type of null); // object
