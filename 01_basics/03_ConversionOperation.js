00a52ca77fc81ba61316a8163dc77b67704ab558
shahzebkhan14 <shazebkhan450331@gmail.com>
2026-07-08 14:04:31 +0000

    conversionOpreration in depth

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -23,5 +23,46 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33;
 let StringNumber = String(someNumber);
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+// ****************************Operations*******************************
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "Hello";
+// let str2 = " World";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log( (3 + 4) * 5 % 3);
+// console.log(+true);
+// console.log(+false);
+
+// true=>1; false=>0
+// +true=>1; +false=>0
+
+// let num1,num2,num3;
+// num1 = num2 = num3 = 2 + 2;
+// console.log(num1,num2, num3);
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+console.log(typeof gameCounter);
+
+
+
+
+
+/ https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
